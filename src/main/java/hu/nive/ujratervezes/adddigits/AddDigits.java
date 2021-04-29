d0904ecf01ba147b659993460e9d37b1be464c10
huszarpp <huszarpp@gmail.com>
2021-04-29 10:15:46 +0200

    Implement AddDigits

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,19 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    int addDigits(String input) {
+
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+
+        int sumOfDigits = 0;
+        for (char c : input.toCharArray()) {
+            int intValueOfCurrentDigit = c - '0';
+            if (0 <= intValueOfCurrentDigit && intValueOfCurrentDigit <= 10) {
+                sumOfDigits += intValueOfCurrentDigit;
+            }
+        }
+        return sumOfDigits;
+    }
 }
